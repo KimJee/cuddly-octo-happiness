a368a717ab5a861d2e3c528c00b556b04aa6d12f
Jeehoon Kim <jeehoon.jk75@gmail.com>
2020-07-12 19:45:42 -0700

    added a gitignore for my javascript

--- LearningNodeJS/Bot.js
+++ LearningNodeJS/Bot.js
@@ -1,4 +1,17 @@
-function myFunction()
-{
-    return;
-}
+console.log("Jee's Bot is starting");
+
+// Grabs the installed package from NPM and allows us to use it.
+
+// Link to the twit documentation
+// https://www.npmjs.com/package/twit
+var Twit = require('twit');
+
+
+var T = new Twit({
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  })
